8b59438e0ef109fe63787a2bd2418967cb70bb9f
0e1 <0e1@riseup.net>
2023-04-14 17:49:42 -0300

    exibe seletores para isolevel=1,2. Usa templates em php. #26

---
+++ postal/select/index.php
@@ -0,0 +1,46 @@
+<?php
+function getTemplate($template, $dir = "templates/")
+{
+  $f = $dir.$template;
+  $c = '';
+
+  if (is_file($f))
+  {
+    $c = file_get_contents($f);
+  }
+
+  return $c;
+};
+
+function parseTemplate($template, $arr)
+{
+  foreach ($arr as $a => $b)
+  {
+    $template = str_replace('{'.$a.'}',$b,$template);
+  }
+  return $template;
+};
+
+$x = str_replace("/","",$_SERVER['REQUEST_URI']);
+
+$xx = explode("-", $x);
+
+$arrTags = array(
+	'country'=>$xx[0]
+);
+
+switch ($xx[0]) {
+  case "BR":
+    echo parseTemplate(getTemplate('pt.html'),$arrTags);
+    echo getTemplate('common.html');
+    break;
+  case "CO":
+    echo parseTemplate(getTemplate('es.html'),$arrTags);
+    echo getTemplate('common.html');
+    break;
+  default:
+    echo parseTemplate(getTemplate('en.html'),$arrTags);
+    echo getTemplate('common.html');
+    break;
+};
+?>
