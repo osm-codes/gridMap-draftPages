e39361b2ae43126f77ed61d02540821f6809d3eb
0e1 <0e1@riseup.net>
2025-06-26 23:33:49 -0300

    Update version swagger. AddressForAll/suporte#65 AddressForAll/WS#41

--- swagger/swagger-initializer.js
+++ swagger/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://api.afa.codes",
+    url: "https://api." + window.location.hostname + "/",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
